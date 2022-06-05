e89624ae06b67626b8d212cef36cbcac3f8fc2f2
Miles Johnson <mgj2@cecs.pdx.edu>
2022-06-05 16:50:10 -0700

    fixed client receiving

--- client.py
+++ client.py
@@ -15,27 +15,25 @@ def send(client, msg):
     message = msg.encode(FORMAT)
     client.send(message)
     if message == '!q':
-        os._exit(1)
+        exit(1)
 
     
 def receive(client):
     resp = ''
     while True:
-            resp = client.recv(2048).decode(FORMAT)
-            if resp != 'quit':
-                print(resp)
-            elif resp=='quit':
-                os._exit(1) 
-            break          
+        resp = client.recv(2048).decode(FORMAT)
+        if resp != 'quit':
+            print(resp)
+        elif resp == 'quit':
+            break
         
-
 
 def user_naming(client):
     print("Enter your name: ")
     name = input()
     send(client, name)
     try:
-        flag = client.recv(2048).decode(FORMAT)
+        flag = client.recv(21).decode(FORMAT)
        
     except ConnectionResetError:
         print('The server is not responding and crashed, please try after sometime, disconnecting the session now')
@@ -44,6 +42,7 @@ def user_naming(client):
         print('The chat server is not available, disconnecting the session now')
         exit(1)
     return flag
+
 
 def main():
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -75,11 +74,4 @@ def main():
 
 
 if __name__ == "__main__":
-    try:
-        main()
-    except KeyboardInterrupt:
-        print("Interrupted")
-        try:
-            sys.exit(1)
-        except SystemExit:
-            os._exit(1)
+    main()
